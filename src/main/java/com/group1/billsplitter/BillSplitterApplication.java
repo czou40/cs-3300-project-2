9e03285ccdd4d577da635b6c51698d1f3ea45cf2
Mukhtar Kussaiynbekov <mukhtarkussaiynbekov@gmail.com>
2021-11-14 13:19:46 -0500

    connect Google Cloud MySQL database

--- src/main/java/com/group1/billsplitter/BillSplitterApplication.java
+++ src/main/java/com/group1/billsplitter/BillSplitterApplication.java
@@ -2,12 +2,39 @@ package com.group1.billsplitter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
+import com.google.api.gax.paging.Page;
+import com.google.cloud.storage.Bucket;
+import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.StorageOptions;
+
+import java.util.List;
+import java.util.Map;
+
 
 @SpringBootApplication
 public class BillSplitterApplication {
 
+//	public List<Map<String, Object>> listUsers() {
+//		return jdbcTemplate.queryForList("SELECT * FROM user;");
+//	}
+
+//	static void authImplicit() {
+//		// If you don't specify credentials when constructing the client, the client library will
+//		// look for credentials via the environment variable GOOGLE_APPLICATION_CREDENTIALS.
+//		Storage storage = StorageOptions.getDefaultInstance().getService();
+//
+//		System.out.println("Buckets:");
+//		Page<Bucket> buckets = storage.list();
+//		for (Bucket bucket : buckets.iterateAll()) {
+//			System.out.println(bucket.toString());
+//		}
+//	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(BillSplitterApplication.class, args);
+		System.out.println("printing");
+//		authImplicit();
 	}
 
 }
