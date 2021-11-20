98da1ec4a55c030cc4e9ff09e241f181d8293cb1
Mukhtar Kussaiynbekov <mukhtarkussaiynbekov@gmail.com>
2021-11-20 17:24:36 -0500

    handle accounts

--- src/main/java/com/group1/billsplitter/WebController.java
+++ src/main/java/com/group1/billsplitter/WebController.java
@@ -1,10 +1,18 @@
 package com.group1.billsplitter;
 
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
+import java.io.*;
 
+import entities.Account;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,10 +24,38 @@ public class WebController {
         this.jdbcTemplate = jdbcTemplate;
     }
 
+    // Account handlers
     @GetMapping("/getUsers")
-    public List<String> getUsers() {
-        return this.jdbcTemplate.queryForList("SELECT * FROM user").stream()
-                .map(m -> m.values().toString())
-                .collect(Collectors.toList());
+    public List<Map<String, Object>> getUsers() {
+        return this.jdbcTemplate.queryForList("SELECT * FROM user");
+    }
+
+    @GetMapping("/getAccounts/{username}")
+    public List<Map<String, Object>> getAccounts(@PathVariable String username) {
+        return this.jdbcTemplate.queryForList("SELECT * FROM accounts WHERE username = ?", username);
+    }
+
+    @PostMapping("/addAccount")
+    public void addAccount(@RequestBody Account newAccount) {
+        jdbcTemplate.update(
+                "INSERT INTO accounts (username, paymentMethod, details) VALUES (?, ?, ?)",
+                newAccount.getUsername(), newAccount.getPaymentMethod(), newAccount.getDetails()
+        );
+    }
+
+    @PutMapping("/updateAccount")
+    public void updateAccount(@RequestBody Account newAccount) {
+        jdbcTemplate.update(
+                "UPDATE accounts SET details = ? WHERE username = ? AND paymentMethod = ?",
+                newAccount.getDetails(), newAccount.getUsername(), newAccount.getPaymentMethod()
+        );
+    }
+
+    @DeleteMapping("/deleteAccount")
+    public void deleteAccount(@RequestBody Account account) {
+        jdbcTemplate.update(
+                "DELETE FROM accounts WHERE username = ? AND paymentMethod = ?",
+                account.getUsername(), account.getPaymentMethod()
+        );
     }
 }
